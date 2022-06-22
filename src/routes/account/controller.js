41e6093bc47f3228fb618c1161e1f66330436799
Ali <alimdv74.am@outlook.com>
2022-06-22 21:31:13 +0430

    hash password for registration by bcrypt

--- src/routes/account/controller.js
+++ src/routes/account/controller.js
@@ -1,15 +1,35 @@
 const BaseController = require("./../controller");
+const bcrypt = require("bcrypt");
+const _ = require("lodash");
 
 module.exports = new (class extends BaseController {
     async register(req, res, next) {
-        // const user = await this.User.create({
-        //     data: req.body,
-        // });
+        const newUser = _.pick(req.body, [
+            "first",
+            "last",
+            "password1",
+            "email",
+            "phone_number",
+        ]);
+
+        const salt = await bcrypt.genSalt(10);
+        newUser.password1 = await bcrypt.hash(newUser.password1, salt);
+
+        const createUser = await this.User.create({
+            data: {
+                first: newUser.first ? newUser.first : null,
+                last: newUser.last ? newUser.last : null,
+                password: newUser.password1,
+                email: newUser.email,
+                phone_number: newUser.phone_number,
+            },
+        });
+
         return this.response({
             res,
             message: "Register Successfully",
             code: 201,
-            data: req.body,
+            data: _.omit(createUser, ["password"]),
         });
     }
 })();
