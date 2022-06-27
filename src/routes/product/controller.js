5a06a632e157a0a974f015c909c561c25f2e7a6b
Ali <alimdv74.am@outlook.com>
2022-06-28 00:39:23 +0430

    add productList and productSingle endpoint to product route

--- src/routes/product/controller.js
+++ src/routes/product/controller.js
@@ -1,3 +1,35 @@
 const BaseController = require("./../controller");
 
-module.exports = new (class extends BaseController {})();
+module.exports = new (class extends BaseController {
+    async productList(req, res, next) {
+        const products = await this.prisma.book.findMany({
+            include: {
+                category: true,
+                author: true,
+            },
+        });
+        this.response({
+            res,
+            message: "All Products",
+            data: products,
+        });
+    }
+
+    async productSingle(req, res, next) {
+        const product = await this.prisma.book.findUnique({
+            where: {
+                slug: req.params.slug,
+            },
+            include: {
+                category: true,
+                author: true,
+            },
+        });
+        if (!product) return next();
+        this.response({
+            res,
+            message: "All Products",
+            data: product,
+        });
+    }
+})();
--- src/routes/product/index.js
+++ src/routes/product/index.js
@@ -1,3 +1,15 @@
 const router = require("express").Router();
+const controller = require("./controller");
+const validator = require("./validator");
+
+router.get(
+    "/", 
+    controller.productList
+);
+
+router.get(
+    "/:slug", 
+    controller.productSingle
+);
 
 module.exports = router;
