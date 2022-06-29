7348debc60785be787e6a8ff30023016a576bde2
Ali <alimdv74.am@outlook.com>
2022-06-29 15:13:49 +0430

    fix bugs in register validator for unique field

--- src/routes/account/validator.js
+++ src/routes/account/validator.js
@@ -1,4 +1,6 @@
 const { check } = require("express-validator");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 module.exports = new (class {
     register() {
@@ -45,7 +47,16 @@ module.exports = new (class {
                 .withMessage("Email format is wrong")
                 .bail()
                 .isLength({ max: 60 })
-                .withMessage("max character for Email is 60"),
+                .withMessage("max character for Email is 60")
+                .custom(async (value) => {
+                    const user = await prisma.user.findUnique({
+                        where: {
+                            email: value,
+                        },
+                    });
+                    if (user) throw new Error("Email Already Exist");
+                    return value;
+                }),
 
             check("phone_number")
                 .notEmpty()
@@ -55,7 +66,16 @@ module.exports = new (class {
                 .withMessage("max character for email is 15")
                 .bail()
                 .isMobilePhone()
-                .withMessage("Phone Number format is wrong"),
+                .withMessage("Phone Number format is wrong")
+                .custom(async (value) => {
+                    const user = await prisma.user.findUnique({
+                        where: {
+                            phone_number: value,
+                        },
+                    });
+                    if (user) throw new Error("Phone Number Already Exist");
+                    return value;
+                }),
         ];
     }
 
@@ -68,33 +88,31 @@ module.exports = new (class {
                 .isEmail()
                 .withMessage("Email format is wrong"),
 
-            check("password")
-                .notEmpty()
-                .withMessage("Password is required"),
+            check("password").notEmpty().withMessage("Password is required"),
         ];
     }
 
-    reset_password(){
+    reset_password() {
         return [
             check("email")
-            .notEmpty()
-            .withMessage("Email is required")
-            .bail()
-            .isEmail()
-            .withMessage("Email format is wrong"),
-        ]
+                .notEmpty()
+                .withMessage("Email is required")
+                .bail()
+                .isEmail()
+                .withMessage("Email format is wrong"),
+        ];
     }
 
-    confirm_reset_password(){
+    confirm_reset_password() {
         return [
             check("password1")
-            .notEmpty()
-            .withMessage("Password is required")
-            .bail()
-            .isStrongPassword({ minLength: 8, maxLength: 128 })
-            .withMessage(
-                "Password must be 8 character contain at least (one uppercase,one lower case,one special character and one number)"
-            ),
+                .notEmpty()
+                .withMessage("Password is required")
+                .bail()
+                .isStrongPassword({ minLength: 8, maxLength: 128 })
+                .withMessage(
+                    "Password must be 8 character contain at least (one uppercase,one lower case,one special character and one number)"
+                ),
 
             check("password2")
                 .notEmpty()
@@ -108,6 +126,6 @@ module.exports = new (class {
                     }
                     return true;
                 }),
-        ]
+        ];
     }
 })();
